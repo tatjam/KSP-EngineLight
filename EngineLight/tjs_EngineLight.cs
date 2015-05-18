025a24abc0277f46a9f8a3d400508097fde29e40
Tatjam <tajampi@gmail.com>
2015-05-18 17:34:13 +0200

    Fixed Class Names

---
+++ EngineLight/tjs_EngineLight.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *          [KSP] Engine Light Mod 
+ *          Made by Tatjam (Tajampi)  
+ *                TajamSoft
+ *--------------------------------------------------
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EngineLight
+{
+    public class tjs_EngineLight : PartModule
+    {
+
+
+    }
+}
