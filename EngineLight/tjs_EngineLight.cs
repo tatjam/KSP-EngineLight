7b492cdebe3b8136f8fbcdbdbe3b56fa553f1b6f
Tatjam <tajampi@gmail.com>
2015-05-18 18:22:01 +0200

    Finished first EngineLight module pass (Testing)

--- EngineLight/tjs_EngineLight.cs
+++ EngineLight/tjs_EngineLight.cs
@@ -32,12 +32,16 @@ namespace EngineLight
 
         [KSPField]
         public float lightPower = 1.0f;  //LightSource power (get's a porcentage based on thrust)
-        
+
+        [KSPField]
+        public float lightRange = 15.0f; //Changes with thrust
+
         //Not config-able until i know how to handle colors in config...
 
         public Color lightColor = new Color(1, 0.88f, 0.68f); //A light orange color
 
-        
+        public Light engineLight = null; //On minus!
+
         //Thanks pizzaoverhead for this!: 
 
         public ModuleEngines _engineModule = null;
@@ -64,23 +68,25 @@ namespace EngineLight
 
                 //Make lights: 
 
-                GameObject engineLight = new GameObject();
-                engineLight.AddComponent<Light>();
+                GameObject TengineLight = new GameObject();
+                TengineLight.AddComponent<Light>();
 
                 //Light Settings:
 
-                engineLight.light.type = LightType.Point;
-                engineLight.light.range = 0.0f; //For now, changes later!
-                engineLight.light.color = lightColor;
-                engineLight.light.intensity = 0.0f; //By default!
-                engineLight.light.enabled = false; //For now!
+                TengineLight.light.type = LightType.Point;
+                TengineLight.light.range = lightRange; //For now, changes later!
+                TengineLight.light.color = lightColor;
+                TengineLight.light.intensity = 0.0f; //By default!
+                TengineLight.light.enabled = false; //For now!
 
                 //Transform Settings:
 
-                engineLight.transform.parent = engineModule.light.transform;
-                engineLight.transform.forward = engineModule.light.transform.forward; //not really required
-                engineLight.transform.position = engineModule.light.transform.position;
+                TengineLight.transform.parent = engineModule.light.transform;
+                TengineLight.transform.forward = engineModule.light.transform.forward; //not really required
+                TengineLight.transform.position = engineModule.light.transform.position;
 
+                engineLight = TengineLight.light;
+                
                 //Done!
             }
             catch(Exception exception)
@@ -90,6 +96,46 @@ namespace EngineLight
             
         }
 
+
+        public override void OnUpdate()
+        {
+            try
+            {
+
+
+                if (engineLight != null)  //Make sure the light exists!
+                {
+
+                    //Check for engine activity:
+                    if(engineModule.isActiveAndEnabled == true && engineLight.enabled == false)
+                    {
+                        engineLight.enabled = true;
+                    }
+                    else
+                    {
+                        if(engineLight.enabled == true) //Not sure if this is required...
+                        {
+                            engineLight.enabled = false;
+                        }
+                    }
+
+                    //Update light status:
+                    if (engineLight.enabled == true)
+                    {
+                        //Intensity = lightIntensity / 100 * thrust  (Porcentage)
+                        
+                            engineLight.intensity = (lightPower / 100) * engineModule.thrustPercentage;
+                            engineLight.range = (lightRange / 100) * engineModule.thrustPercentage;
+
+                    }
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.LogError("[EngineLight] Error onUpdate: " + ex.Message);
+            }
+        }
         //Useful, checks for debug before printing, do not use if message is important
         public void print(object text)
         {
