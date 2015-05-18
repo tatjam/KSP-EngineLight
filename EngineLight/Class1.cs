d40834006ad449095a85ffe94f213526bf44ba87
Tatjam <tajampi@gmail.com>
2015-05-18 17:27:34 +0200

    Init!

---
+++ EngineLight/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineLight
+{
+    public class Class1
+    {
+    }
+}
