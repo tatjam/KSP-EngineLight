4f3b0d989f0a462229accc4798b551b4fbda44a2
Tatjam <tajampi@gmail.com>
2015-06-16 20:02:39 +0200

    Added disable on IVA!

---
+++ EngineLight/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineLight
+{
+    //Not a MonoBehaviour!
+    class Utils
+    {
+        //As the name says, checks if the user is using the IVA camera
+        //Added the OR just in case that other camera mode is also IVA!
+        public static bool isIVA()
+        {
+             return CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.IVA || CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.Internal;
+        }
+
+
+
+
+    }
+}
